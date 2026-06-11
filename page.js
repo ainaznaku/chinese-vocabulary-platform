b2934b1aaac4b8ec3d1eecf7b74ccf8121c4878f
ainaznaku <ainaznaku@gmail.com>
2026-06-11 23:24:05 +0800

    Add files via upload

---
+++ page.js
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Chinese Vocabulary',
+  description: 'Chinese vocabulary learning app'
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="zh">
+      <body>{children}</body>
+    </html>
+  );
+}
